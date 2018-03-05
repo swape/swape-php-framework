33c374721def54477ee4fd16e803e8802d2b511a
Alireza Balouch <swape@swape.net>
2018-03-05 22:16:05 +0100

    adding getallheaders for non apache header

--- sf/index.php
+++ sf/index.php
@@ -1,4 +1,17 @@
 <?php
+// this is in case we run other than apache
+if (!function_exists('getallheaders')) {
+    function getallheaders()
+    {
+        $headers = [];
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
 
 function startFramework()
 {
