d000243b07c0902c9dc8e302869897bc7d82bbd3
Alireza Balouch <swape@swape.net>
2018-03-13 14:39:04 +0100

    simplify the query and insert SQL

--- web/app/controller/api.php
+++ web/app/controller/api.php
@@ -4,31 +4,22 @@ class sf_apiClass
 {
     public function sf_index($arr)
     {
-        $strSQL = 'SELECT * FROM `test_table` ';
-        $objPrepare = $arr['db']->prepare($strSQL);
-        $objPrepare->setFetchMode(PDO::FETCH_ASSOC);
-        $objPrepare->execute();
-        $arrData = $objPrepare->fetchAll();
-
-        return $arrData;
+        $result = $arr['db']->query('SELECT * FROM `test_table`');
+        return $result;
     }
 
     public function sf_insert($arr)
     {
-        if ($arr['method'] == 'POST') {
+        if ($arr['method'] == 'POST' && isset($arr['data']['req']['myvar'])) {
             $strSQL = "INSERT INTO test_table SET text = :mytext ";
+            $arrParams = [
+              ['name'=>':mytext' , 'value'=> $arr['data']['req']['myvar']]
+            ];
 
-            $sth = $arr['db']->prepare($strSQL);
-            $sth->bindParam(':mytext', $arr['data']['req']['myvar']);
-            $sth->execute();
-            $error = $sth->errorInfo();
-            if (isset($error[0]) && $error[0] == '00000') {
-                return ['id'=> $arr['db']->lastInsertId()];
-            } else {
-                return ['error'=> $sth->errorInfo()];
-            }
+            $result = $arr['db']->query($strSQL, $arrParams);
+            return ['myvar'=> $result];
         } else {
-            return ['method'=> $arr['method'] ];
+            return ['method'=> $arr['method'] ,'data'=> $arr['data']];
         }
     }
 }
