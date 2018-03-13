2254e7300f923d2a10dfb3fd5b549e7d9ffbd167
Alireza Balouch <swape@swape.net>
2018-03-13 14:39:51 +0100

    using the new improved return object

--- web/app/template/index.php
+++ web/app/template/index.php
@@ -10,14 +10,21 @@
 
     <div class="ui message">
       <div class="header">
-        template: /template/index.php
+        Template: /web/app/template/index.php
       </div>
-      <p>myvar data from controller:
+      <p>myvar data from controller in /web/app/controller/index.php:
 
         <?php
-        foreach ($data['myvar'] as $value) {
-            echo "<div>${value['id']}: ${value['text']}</div>";
+        if (isset($data['myvar']['error']) and $data['myvar']['error'][0] !== '00000') {
+            echo '<pre>';
+            print_r($data['myvar']['error']);
+            echo '</pre>';
+        } else {
+            foreach ($data['myvar']['data'] as $value) {
+                echo "<div>${value['id']}: ${value['text']}</div>";
+            }
         }
+
         ?>
 
       </p>
