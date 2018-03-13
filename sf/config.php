df716c07f0d193ac033114eaab923b0c92ef3268
Alireza Balouch <swape@swape.net>
2018-03-13 14:38:18 +0100

    using pdo wrapper instead of getDBObj function

--- sf/config.php
+++ sf/config.php
@@ -3,16 +3,7 @@
 ini_set('display_errors', 'on');
 error_reporting(E_ALL);
 
-$arrVars['some_info_from_config'] = 'hello';
+require_once('PDOWrapperClass.php');
 
-// Return db obj
-function getDbObj()
-{
-    // se documentation on http://php.net/manual/en/book.pdo.php
-    // mysql
-    return new PDO('mysql:host=localhost;dbname=testdb', 'root', 'root');
-    // postgresql
-    // return new PDO('pgsql:host=192.168.137.1;port=5432;dbname=anydb', 'anyuser', 'pw');
-    // no db
-    // return false;
-}
+$arrVars['some_info_from_config'] = 'hello';
+$arrVars['db'] = new PDOWrapperClass('mysql:host=localhost;dbname=testdb', 'root', 'root');
